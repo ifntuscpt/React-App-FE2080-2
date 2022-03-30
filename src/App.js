7ba3e6229fb450814ccd23d3742d1b4206995365
ifntuscpt <95.ifentiusciputra@gmail.com>
2022-03-30 14:37:52 +0700

    Spotify Auth API and search song functionalities with Spotify Search API

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import './App.css';
-import data from './data';
-import Card from './components/Card';
+import Home from './Pages/Home';
 
 function App() {
-  const renderCard = () => data.map((v) => <Card key={v.album.artists[0].id} img={v.album.images[0].url} album={v.album.name} name={v.album.artists[0].name}/>)
   return (
-    <div className="header">
-    <h1>Playlist</h1>
-      <div className='album'>
-        {renderCard()}
-        <button class="myButton"><a href={data.uri}>Select</a></button>
-      </div>
-    </div>
+    <Home />
   );
 }
 
