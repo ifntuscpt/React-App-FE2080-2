cd829f828ba618f4e0bd9d8873499728169f3650
ifntuscpt <95.ifentiusciputra@gmail.com>
2022-04-08 07:57:55 +0700

    Adding Router

---
+++ src/components/Logo/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.css';
+
+export default function Logo() {
+  return (
+    <div className="logo">
+      <h2>CreatePlaylist Spotify App</h2>
+    </div>
+  )
+}
